f361fbfaaa07c27fa48848b5e7f186a2345ebc2f
pamuba <partha.bora24@gmail.com>
2021-07-27 21:20:47 +0530

    256

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import {MatButtonModule} from '@angular/material/button';
 import {MatCardModule} from '@angular/material/card';
+import { MatIconModule } from "@angular/material/icon";
 
 @NgModule({
-    imports:[MatButtonModule, MatCardModule],
-    exports:[MatButtonModule, MatCardModule]
+    imports:[MatButtonModule, MatCardModule, MatIconModule],
+    exports:[MatButtonModule, MatCardModule, MatIconModule]
 })
 export class MaterialModule{}
